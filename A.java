fb47498c3f956106db241a2a1cc62be3c730e1b7
Juliano Godinho <32497229+JulianoA28@users.noreply.github.com>
2020-11-19 10:45:14 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,44 @@
+//   Trabalho Prático de Gerencia de Configurações - Arquivo Java
+//   GCC188 - Engenharia de Software
+//   Nome: Juliano Expedito de Andrade Godinho
+//   Turma: 10A
+//   Matricula: 201811302
+
+// Classe A
+public class A {
+	// Atributos
+	private int A1;
+	private float A2;
+	
+	// SET A1
+	public void setA1(int valor) {
+		this.A1 = valor;
+	}
+	
+	// SET A2
+	public void setA2(float valor) {
+		this.A2 = valor;
+	}
+	
+	// GET A1
+	public int getA1() {
+		return A1;
+	}
+	
+	// GET A2
+	public float getA2() {
+		return A2;
+	}
+	
+	// Metodo MA1
+	public void MA1() {
+		System.out.println("MA1");
+	}
+	
+	// Metodo MA2
+	public void MA2() {
+		System.out.println("MA2");
+	}
+}
+
+
---
+++ B.py
@@ -0,0 +1,35 @@
+#   Trabalho Prático de Gerencia de Configurações - Arquivo Python
+#   GCC188 - Engenharia de Software
+#   Nome: Juliano Expedito de Andrade Godinho
+#   Turma: 10A
+#   Matricula: 201811302
+
+# Classe B
+def B():
+	# Atributos
+	B1 = None # Int
+	B2 = None # Float
+	
+	# SET B1
+	def setB1(self, valor):
+		self.B1 = valor
+		
+	# SET B2
+	def setB2(self, valor):
+		self.B2 = valor
+		
+	# GET B1
+	def getB1(self):
+		return self.B1
+		
+	# GET B2
+	def getB2(self):
+		return self.B2
+		
+	# Metodo MB1
+	def MB1():
+		print("MB1")
+	
+	# Metodo MB2
+	def MB2():
+		print("MB2")
