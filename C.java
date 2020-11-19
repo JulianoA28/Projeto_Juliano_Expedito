e2746c57701ca1f1fa3e8a5f10a5c8c63bf96611
Juliano Expedito <julianoeagodinho6@gmail.com>
2020-11-19 12:50:49 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,19 @@
+
+// Classe C
+public class C {
+	// Atributos
+	private String C1;
+	private int C2;
+	
+	// Metodo MC1
+	public void MC1() {
+		System.out.println("MC1");
+	}
+	
+	// Metodo MC2
+	public void MC2() {
+		System.out.println("MC2");
+	}
+
+}
+
